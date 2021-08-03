0122c59d381b3a183a77b31fde64c04fa38bec97
Julián Swerdlin <julianswer36@gmail.com>
2021-08-03 17:46:14 -0300

    last files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
       {
-        name: 'Canalizados',
+        name: 'Canalizados-Test',
         exec_mode: 'cluster',
         instances: 'max', // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
