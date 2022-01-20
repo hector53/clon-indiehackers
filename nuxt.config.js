924e84ad16bb994a95f70a70d20f6d67ed4d2324
hector53 <hectoracosta5@gmail.com>
2022-01-20 18:45:56 -0400

    20enerofin2

--- nuxt.config.js
+++ nuxt.config.js
@@ -199,7 +199,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    /*
+    
    extractCSS: true,
    optimization: {
     splitChunks: {
@@ -213,7 +213,7 @@ export default {
       }
     }
   },
-*/
+
  //analyze: true,
    
   }
